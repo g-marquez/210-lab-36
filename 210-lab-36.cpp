272b7463fd4c0acadc0f571fb1a4636782f07980
Gabe Marquez <gabrielmarquez98@gmail.com>
2026-05-10 13:40:58 -0700

    Milestone 1: starting lab 36 with starter code files provided in Canvas

---
+++ 210-lab-36.cpp
@@ -0,0 +1,15 @@
+//*****************************************************************************
+// COMSC-210 | Lab 36 | Gabriel Marquez
+// Description: this program creates and populates a BST from records contained
+// in the codes.txt file.
+//*****************************************************************************
+
+#include <iostream>
+#include "IntBinaryTree.h"
+using namespace std;
+
+int main() {
+    
+
+    return 0;
+}
