18a9d520e53cc498fca2808a15c1e5ef2afc4b69
Gabe Marquez <gabrielmarquez98@gmail.com>
2026-05-10 14:10:40 -0700

    update

--- 210-lab-36.cpp
+++ 210-lab-36.cpp
@@ -5,21 +5,29 @@
 //*****************************************************************************
 
 #include <iostream>
+#include <string>
+#include <fstream>
 #include "StrBinaryTree.h"
 using namespace std;
 
-int main() {
-    //test code for Milestone 2
-    StrBinaryTree tree;
-    tree.insertNode("aaa");
-    tree.insertNode("bbb");
-    tree.insertNode("ccc");
-    tree.insertNode("ddd");
-    tree.insertNode("eee");
+string FILE_NAME = "codes.txt";
 
-    // Display the values.
-   cout << "Here are the values in the tree:\n";
-   tree.displayInOrder();
+int main() {
+    //decare empty BST
+    StrBinaryTree tree;
+    
+    //populate with strings from codes.txt
+    string input;
+    ifstream fin(FILE_NAME);
+    if (fin.good( )) {
+        while (fin >> input)
+            tree.insertNode(input);
+        fin.close( );
+    }
+    else {
+        cout << "ERROR! Please verify file name/directory and restart program.";
+        return 1;
+    }
 
     return 0;
 }
